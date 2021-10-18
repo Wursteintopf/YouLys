f7759ffa14d659a9d699ff2d00f55b4f6d947e56
Wursteintopf <markus@vogelvlug.de>
2021-10-18 14:01:26 +0200

    Fix small bug, actually load API from https not http and rename Title

--- src/shared/paths.ts
+++ src/shared/paths.ts
@@ -4,7 +4,7 @@ let baseUrlBuilder = ''
 if (hostname === 'youlys.de') {
   baseUrlBuilder = baseUrlBuilder + 'https://' + hostname + ':3012'
 } else if (hostname === 'dev.youlys.de') {
-  baseUrlBuilder = baseUrlBuilder + 'http://' + hostname + ':3011'
+  baseUrlBuilder = baseUrlBuilder + 'https://' + hostname + ':3011'
 } else {
   baseUrlBuilder = baseUrlBuilder + 'http://' + hostname + ':3001'
 }
