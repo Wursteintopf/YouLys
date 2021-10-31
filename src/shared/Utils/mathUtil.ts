9e07b038ca3045c47b2b0a77a714d810c83b1c87
Wursteintopf <markus@vogelvlug.de>
2021-10-31 16:28:01 +0100

    Change success_factor calculation from average to median and fix edge cases to Inf and NaN get impossible

---
+++ src/shared/Utils/mathUtil.ts
@@ -0,0 +1,13 @@
+export const median = (values: number[]) => {
+  if (values.length === 0) throw new Error('No inputs')
+
+  const sorted = values.slice().sort((a, b) => a - b)
+
+  const middle = Math.floor(sorted.length / 2)
+
+  if (sorted.length % 2 === 0) {
+    return (sorted[middle - 1] + sorted[middle]) / 2
+  }
+
+  return sorted[middle]
+}
